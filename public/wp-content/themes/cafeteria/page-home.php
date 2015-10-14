a6d78529b9713e067062e489826337af47b5dcfb
wl-ak <wl-ak@wonderslab.com>
2015-10-14 13:25:31 +0300

    Fixing circle image popup

--- public/wp-content/themes/cafeteria/page-home.php
+++ public/wp-content/themes/cafeteria/page-home.php
@@ -57,7 +57,7 @@ get_header(); ?>
                     <?php $i++; ?>
                         <div class="col-3">
                                 <?php $url = wp_get_attachment_url( get_post_thumbnail_id($post->ID) ); ?>
-                            <div class="circle" style="background-image: url('<?php echo $url; ?>'); background-position: 50% 52%; background-repeat: no-repeat; background-size: 77% auto;">
+                            <div class="circle" style="background-image: url('<?php echo $url; ?>'); background-position: 50% 52%; background-repeat: no-repeat; background-size: 100% auto;">
                                 <div class="img"></div>
                             </div>
                             <h2 class="firstfont caption colormain" style="top: 148px;"><?php the_title(); ?></h2>
