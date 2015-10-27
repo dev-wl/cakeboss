43ac6b13dacad449f50707412e8e692c1b6fe6d1
wl-ak <wl-ak@wonderslab.com>
2015-10-27 15:21:30 +0200

    Fixing short pages scroll

--- public/wp-content/themes/cafeteria/header.php
+++ public/wp-content/themes/cafeteria/header.php
@@ -86,7 +86,7 @@
 
             h = $('header.cf').height();
             if($(window).height() < 990) {
-                h += 10;
+                h += 100;
             }
             if($('#wpadminbar').css('display') == 'block') {
                 h += $('#wpadminbar').height();
