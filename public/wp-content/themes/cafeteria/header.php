6e74d8662afa60c2332c6b6ddab2d4a673fd58cf
wl-ak <wl-ak@wonderslab.com>
2015-10-27 17:35:07 +0200

    Scroll fixing

--- public/wp-content/themes/cafeteria/header.php
+++ public/wp-content/themes/cafeteria/header.php
@@ -86,7 +86,7 @@
 
             h = $('header.cf').height();
             if($(window).height() < 990) {
-                h += 50;
+                h += 60;
             }
             if($('#wpadminbar').css('display') == 'block') {
                 h += $('#wpadminbar').height();
