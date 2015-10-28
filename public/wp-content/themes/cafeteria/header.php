8599f69d596d9940332b4ef8687b2ee1c3a40c5e
wl-ak <wl-ak@wonderslab.com>
2015-10-28 13:11:58 +0200

    Fixing mobile menu

--- public/wp-content/themes/cafeteria/header.php
+++ public/wp-content/themes/cafeteria/header.php
@@ -108,6 +108,7 @@
                     e.preventDefault();
                 }
 
+                console.log('t = ' +target);
                 if(target.is('#menu-mobile-menu li:eq(10)') || target.is('#menu-mobile-menu li:eq(6)'))
                     $('#menu-mobile-menu li:eq(0), #menu-mobile-menu li:eq(1)').toggle("slow");
             });
