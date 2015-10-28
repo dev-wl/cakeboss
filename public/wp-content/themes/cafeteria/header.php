03b586c6b6ac8321f9a735dc661ba0b37562c748
wl-ak <wl-ak@wonderslab.com>
2015-10-28 13:40:04 +0200

    Fixing mobile menu

--- public/wp-content/themes/cafeteria/header.php
+++ public/wp-content/themes/cafeteria/header.php
@@ -104,17 +104,9 @@
         if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
             $('#menu-mobile-menu li:eq(0), #menu-mobile-menu li:eq(2), #menu-mobile-menu li:eq(10)').click(function(e) {
                 var target = $( e.target );
-
-                console.log(e.target.nodeName);
-
-
                 if ( !target.is( "ul.sub-menu li a" ) ) {
                     e.preventDefault();
                 }
-
-
-                if(target.is('#menu-mobile-menu li:eq(10)') || target.is('#menu-mobile-menu li:eq(6)'))
-                    $('#menu-mobile-menu li:eq(0), #menu-mobile-menu li:eq(1)').toggle("slow");
             });
         }
 
