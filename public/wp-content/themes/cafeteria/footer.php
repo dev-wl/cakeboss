b6fac5ebb698c1bdb29d5c0cc40286374c68df17
wl-ak <wl-ak@wonderslab.com>
2015-11-12 17:09:29 +0200

    Fixing ipad menu closing

--- public/wp-content/themes/cafeteria/footer.php
+++ public/wp-content/themes/cafeteria/footer.php
@@ -1,7 +1,7 @@
         <section class="footer footer-small">
             <div class="footerinfo-wrapper">
-                <div class="footer-left">FOLLOW US ON <a href="http://twitter.com"><img src="/wp-content/themes/cafeteria/css/images/twitter-circle-white.png" class="tw-icon"/></a> <a href="http://facebook.com"><img src="/wp-content/themes/cafeteria/css/images/facebook.png" class="fb-icon"/></a> <a href="/privacy-policy"><p class="privacy">Privacy Policy</p></a></div>
-                <div class="footer-right"><span class='tlc-logo'>T</span><span class='tlc-logo'>L</span><span class='tlc-logo'>C</span> &copy;2015 and other stuff that goes here and other stuff that goes here and other stuff that goes here</div>
+                <div class="footer-left">FOLLOW US ON <a href="https://twitter.com/cakebosscoffee"><img src="/wp-content/themes/cafeteria/css/images/twitter-circle-white.png" class="tw-icon"/></a> <a href="https://www.facebook.com/cakebosscoffee/"><img src="/wp-content/themes/cafeteria/css/images/facebook.png" class="fb-icon"/></a> <a href="/privacy-policy"><p class="privacy">Privacy Policy</p></a></div>
+                <div class="footer-right"><span class='tlc-logo'>T</span><span class='tlc-logo'>L</span><span class='tlc-logo'>C</span> &copy;2015 Discovery Communications, LLC. TLC, CakeBoss and related  logos are trademarks of Discovery Communications, LLC, used under license. All rights reserved. tlc.com/cake-boss</div>
 
                 <div class="clearfix"></div>
             </div>
