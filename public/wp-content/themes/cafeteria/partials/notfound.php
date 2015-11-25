9d40c93ec1461d4b08b6a8323bde842138a98ffb
wl-ak <wl-ak@wonderslab.com>
2015-11-25 10:43:54 +0200

    Slider iphone fixes

--- public/wp-content/themes/cafeteria/partials/notfound.php
+++
@@ -1,3 +0,0 @@
-<div class="not-found">
-	<p><?php _e('No results were found for the requested page.', 'aletheme');?></p>
-</div>
--- public/wp-content/themes/cafeteria/sidebar-woocommerce.php
+++
@@ -1,3 +0,0 @@
-<aside class="sidebar cf">
-    <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('woocommerce-sidebar') ) : endif; ?>
-</aside>
